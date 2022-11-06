fb008bfd8424d3b98e47103de560ccd3a40a25a8
Tristan Streichenberger <tristanstreich@berkeley.edu>
2022-11-06 12:43:37 -0800

    basic bevy tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugin(HelloPlugin)
+    .run();
 }
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+        .add_startup_system(add_people)
+        .add_system(greet_people);
+    }
+}
+
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
