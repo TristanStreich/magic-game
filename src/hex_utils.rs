bc39f2bbedf5106c860f4b608cac44b820f2e25a
TristanStreich <tristanstreich@gmail.com>
2022-11-07 08:13:12 -0800

    hex grid as entity with children

--- src/hex_utils.rs
+++ src/hex_utils.rs
@@ -1,21 +1,32 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::Inspectable;
 use std::cmp::{max,min};
+use std::collections::HashMap;
 
 pub const HEX_INNER_RADIUS: f32 = 40.0;
 pub const HEX_CIRCUMRADIUS: f32 = HEX_INNER_RADIUS * 1.154700538; //sqrt(4/3)
 pub const HEX_SMALL_DIAMETER: f32 = 2.0 * HEX_INNER_RADIUS;
 pub const HEX_LARGE_DIAMETER: f32 = 2.0 * HEX_CIRCUMRADIUS;
 
-pub const HEX_GRID_RADIUS: i32 = 3;
+pub const HEX_GRID_RADIUS: i32 = 5;
 
 pub const HEX_SPRITE_SCALE: f32 = HEX_SMALL_DIAMETER * 0.00275;
 
+
+
+type WorldCoord = (f32, f32);
 
 #[derive(Component, Inspectable, Debug)]
 pub struct HexCoord(i32,i32);
 
 impl HexCoord {
+
+    pub fn from_world(world_coord: WorldCoord) -> HexCoord {
+        let x = (f32::sqrt(3.0)*world_coord.0 - world_coord.1) / 3.0 / HEX_CIRCUMRADIUS;
+        let y = ((2.0/3.0) * world_coord.1) / HEX_CIRCUMRADIUS;
+        return HexCoord(x.round() as i32, y.round() as i32);
+    }
+
     pub fn to_world(&self) -> WorldCoord {
         let x = HEX_CIRCUMRADIUS * f32::sqrt(3.0) * ((self.0 as f32) + (self.1 as f32) / 2.0);
         let y = HEX_CIRCUMRADIUS * (3.0/2.0) * (self.1 as f32);
@@ -24,7 +35,7 @@ impl HexCoord {
 
     // returns all the hex coords that are
     // within radius number of tiles
-    pub fn in_range(&self, radius: i32) -> Vec<HexCoord> {
+    pub fn within_radius(&self, radius: i32) -> Vec<HexCoord> {
         let mut within = Vec::new();
         for x in -radius..radius+1 {
             for y in max(-radius, (-x)-radius)..min(radius,(-x)+radius)+1 {
@@ -35,33 +46,35 @@ impl HexCoord {
     }
 }
 
-type WorldCoord = (f32, f32);
+pub struct HexGridBundle;
 
-// wrong
-pub fn world_to_hex(world_coord: WorldCoord) -> HexCoord {
-    let x = (f32::sqrt(3.0)*world_coord.0 - world_coord.1) / 3.0 / HEX_CIRCUMRADIUS;
-    let y = ((2.0/3.0) * world_coord.1) / HEX_CIRCUMRADIUS;
-    return HexCoord(x.round() as i32, y.round() as i32);
-}
-
-pub struct HexGrid;
-
-impl Plugin for HexGrid {
+impl Plugin for HexGridBundle {
     fn build(&self, app: &mut App) {
         app
-        .add_startup_system(init_grid);
+        .add_startup_system(HexGrid::spawn);
     }
 }
 
+#[derive(Component, Inspectable)]
+pub struct HexGrid;
 
-fn init_grid(
-    mut commands: Commands,
-    assets: Res<AssetServer>
-) {
-    for hex_coord in HexCoord(0,0).in_range(HEX_GRID_RADIUS).into_iter() {
-        HexTile::spawn_at(hex_coord, &mut commands, &assets)
+ impl HexGrid {
+    fn spawn(
+        mut commands: Commands,
+        assets: Res<AssetServer>
+    ) {
+        let mut tiles = Vec::new();
+        for hex_coord in HexCoord(0,0).within_radius(HEX_GRID_RADIUS).into_iter() {
+            let tile = HexTile::spawn_at(hex_coord, &mut commands, &assets);
+            tiles.push(tile);
+        }
+        commands
+        .spawn_bundle(SpatialBundle{..default()})
+        .insert(Name::new("HexGrid"))
+        .insert(HexGrid)
+        .push_children(&tiles);
     }
-}
+ }
 
 
 #[derive(Component, Inspectable)]
@@ -72,16 +85,17 @@ impl HexTile {
         hex_coord: HexCoord,
         commands: &mut Commands,
         assets: &Res<AssetServer>
-    ) {
+    ) -> Entity {
         let (x, y) = hex_coord.to_world();
         commands.spawn_bundle(SpriteBundle {
-            texture: assets.load("hex_cropped.png"),
+            texture: assets.load("hex.png"),
             transform: Transform::from_xyz(x, y, 0.0)
                         .with_scale(Vec3::new(HEX_SPRITE_SCALE, HEX_SPRITE_SCALE, 1.0)),
             ..default()
         })
         .insert(Name::new("HexTile"))
         .insert(hex_coord)
-        .insert(HexTile);
+        .insert(HexTile)
+        .id()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod hex_utils;
 mod debug;
 
-use hex_utils::HexGrid;
+use hex_utils::HexGridBundle;
 use debug::{DebugPlugin};
 
 use bevy::prelude::*;
@@ -9,7 +9,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
-    .add_plugin(HexGrid)
+    .add_plugin(HexGridBundle)
     .add_plugin(DebugPlugin)
     .add_startup_system_to_stage(StartupStage::PreStartup, spawn_camera)
     .run();
