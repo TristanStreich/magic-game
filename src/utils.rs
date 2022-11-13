64ec56beac399bd11e24e20e3d528b69355ae234
shrav-k <shrav.k97@gmail.com>
2022-11-12 16:56:05 -0800

    organize logic into plugins

---
+++ src/utils.rs
@@ -0,0 +1,6 @@
+pub mod camera;
+pub mod debug;
+pub mod hex;
+pub mod mouse;
+
+pub type WorldCoord = (f32, f32);
--- src/utils/debug.rs
+++ src/utils/debug.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{RegisterInspectable, WorldInspectorPlugin};
 
-use crate::hex_utils::{HexTile, HexCoord};
+use crate::utils::hex::{HexTile, HexCoord};
 
 pub struct DebugPlugin;
 
