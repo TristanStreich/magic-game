77375d388e322488b638e00989b7342908de9aa9
TristanStreich <tristanstreich@gmail.com>
2022-11-18 20:08:23 -0800

    sky and sun

--- src/plugins/world_3d.rs
+++ src/plugins/world_3d.rs
@@ -4,6 +4,8 @@ pub mod camera;
 pub mod hex;
 
 use bevy::app::{PluginGroup, PluginGroupBuilder};
+use bevy::prelude::*;
+use config::{SUN_HEIGHT, SUN_INTENSITY};
 
 pub struct World3dPlugins;
 
@@ -13,5 +15,32 @@ impl PluginGroup for World3dPlugins {
         .add(camera::CameraPlugin)
         .add(hex::HexPlugin)
         .add(debug::DebugPlugin)
+        .add(Sky)
     }
+}
+
+
+
+pub struct Sky;
+
+impl Plugin for Sky {
+    fn build(&self, app: &mut App) {
+        app
+        .insert_resource(ClearColor(Color::rgb(0.5294, 0.8087, 0.9216)))
+        .add_startup_system(spawn_sun);
+    }
+}
+
+fn spawn_sun(
+    mut commands: Commands
+) {
+    commands
+    .spawn(PointLightBundle {
+        transform: Transform::from_xyz(0., SUN_HEIGHT, 0.),
+        point_light: PointLight {
+            intensity: SUN_INTENSITY,
+            ..default()
+        },
+        ..default()
+    });
 }
--- src/plugins/world_3d/config.rs
+++ src/plugins/world_3d/config.rs
@@ -3,7 +3,7 @@ pub const HEX_INNER_RADIUS: f32 = 0.9;
 pub const HEX_CIRCUMRADIUS: f32 = HEX_INNER_RADIUS * 1.154700538; //sqrt(4/3)
 pub const HEX_SMALL_DIAMETER: f32 = 2.0 * HEX_INNER_RADIUS;
 pub const HEX_LARGE_DIAMETER: f32 = 2.0 * HEX_CIRCUMRADIUS;
-pub const HEX_GRID_RADIUS: i32 = 5;
+pub const HEX_GRID_RADIUS: i32 = 25;
 
 
 // Camera
@@ -11,3 +11,9 @@ pub const CAMERA_SENSITIVITY: f32 = 0.00012;
 pub const CAMERA_SPEED: f32 = 12.;
 pub const MAX_PITCH: f32 = 0.95;
 pub const MIN_PITCH: f32 = 0.25;
+
+
+// Sun
+pub const SUN_INTENSITY: f32 = 10_000_000.;
+pub const SUN_RADIUS: f32 = 10_000.;
+pub const SUN_HEIGHT: f32 = 1_000.0;
