1be373b0b8cd5cd7613ae55cd0bc6aa903787f54
TristanStreich <tristanstreich@gmail.com>
2022-11-19 13:43:47 -0800

    trimmed sky box example

--- examples/sky_box.rs
+++ examples/sky_box.rs
@@ -19,49 +19,32 @@ use bevy::{
             TextureViewDescriptor, TextureViewDimension,
         },
         renderer::RenderDevice,
-        texture::{CompressedImageFormats, FallbackImage},
+        texture::FallbackImage,
     },
 };
-
-const CUBEMAPS: &[(&str, CompressedImageFormats)] = &[
-    (
-        "textures/Ryfjallet_cubemap.png",
-        CompressedImageFormats::NONE,
-    ),
-    (
-        "textures/Ryfjallet_cubemap_astc4x4.ktx2",
-        CompressedImageFormats::ASTC_LDR,
-    ),
-    (
-        "textures/Ryfjallet_cubemap_bc7.ktx2",
-        CompressedImageFormats::BC,
-    ),
-    (
-        "textures/Ryfjallet_cubemap_etc2.ktx2",
-        CompressedImageFormats::ETC2,
-    ),
-];
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(MaterialPlugin::<CubemapMaterial>::default())
         .add_startup_system(setup)
-        .add_system(cycle_cubemap_asset)
-        .add_system(asset_loaded.after(cycle_cubemap_asset))
+        // .add_system(cycle_cubemap_asset)
+        .add_system(asset_loaded)
         .add_system(camera_controller)
-        .add_system(animate_light_direction)
+        // .add_system(animate_light_direction)
         .run();
 }
 
 #[derive(Resource)]
 struct Cubemap {
     is_loaded: bool,
-    index: usize,
     image_handle: Handle<Image>,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+) {
     // directional 'sun' light
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -73,7 +56,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..default()
     });
 
-    let skybox_handle = asset_server.load(CUBEMAPS[0].0);
+    let skybox_handle = asset_server.load("sky_box/Ryfjallet_cubemap.png");
     // camera
     commands.spawn((
         Camera3dBundle {
@@ -93,52 +76,10 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     commands.insert_resource(Cubemap {
         is_loaded: false,
-        index: 0,
         image_handle: skybox_handle,
     });
 }
 
-const CUBEMAP_SWAP_DELAY: f32 = 3.0;
-
-fn cycle_cubemap_asset(
-    time: Res<Time>,
-    mut next_swap: Local<f32>,
-    mut cubemap: ResMut<Cubemap>,
-    asset_server: Res<AssetServer>,
-    render_device: Res<RenderDevice>,
-) {
-    let now = time.elapsed_seconds();
-    if *next_swap == 0.0 {
-        *next_swap = now + CUBEMAP_SWAP_DELAY;
-        return;
-    } else if now < *next_swap {
-        return;
-    }
-    *next_swap += CUBEMAP_SWAP_DELAY;
-
-    let supported_compressed_formats =
-        CompressedImageFormats::from_features(render_device.features());
-
-    let mut new_index = cubemap.index;
-    for _ in 0..CUBEMAPS.len() {
-        new_index = (new_index + 1) % CUBEMAPS.len();
-        if supported_compressed_formats.contains(CUBEMAPS[new_index].1) {
-            println!("Swapping Cube Map!");
-            break;
-        }
-        info!("Skipping unsupported format: {:?}", CUBEMAPS[new_index]);
-    }
-
-    // Skip swapping to the same texture. Useful for when ktx2, zstd, or compressed texture support
-    // is missing
-    if new_index == cubemap.index {
-        return;
-    }
-
-    cubemap.index = new_index;
-    cubemap.image_handle = asset_server.load(CUBEMAPS[cubemap.index].0);
-    cubemap.is_loaded = false;
-}
 
 fn asset_loaded(
     mut commands: Commands,
@@ -147,12 +88,10 @@ fn asset_loaded(
     mut meshes: ResMut<Assets<Mesh>>,
     mut cubemap_materials: ResMut<Assets<CubemapMaterial>>,
     mut cubemap: ResMut<Cubemap>,
-    cubes: Query<&Handle<CubemapMaterial>>,
 ) {
     if !cubemap.is_loaded
         && asset_server.get_load_state(cubemap.image_handle.clone_weak()) == LoadState::Loaded
     {
-        info!("Swapping to {}...", CUBEMAPS[cubemap.index].0);
         let mut image = images.get_mut(&cubemap.image_handle).unwrap();
         // NOTE: PNGs do not have any metadata that could indicate they contain a cubemap texture,
         // so they appear as one texture. The following code reconfigures the texture as necessary.
@@ -166,34 +105,15 @@ fn asset_loaded(
             });
         }
 
-        // spawn cube
-        let mut updated = false;
-        for handle in cubes.iter() {
-            if let Some(material) = cubemap_materials.get_mut(handle) {
-                updated = true;
-                material.base_color_texture = Some(cubemap.image_handle.clone_weak());
-            }
-        }
-        if !updated {
-            commands.spawn(MaterialMeshBundle::<CubemapMaterial> {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: 10000.0 })),
-                material: cubemap_materials.add(CubemapMaterial {
-                    base_color_texture: Some(cubemap.image_handle.clone_weak()),
-                }),
-                ..default()
-            });
-        }
+        commands.spawn(MaterialMeshBundle::<CubemapMaterial> {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 10000.0 })),
+            material: cubemap_materials.add(CubemapMaterial {
+                base_color_texture: Some(cubemap.image_handle.clone_weak()),
+            }),
+            ..default()
+        });
 
         cubemap.is_loaded = true;
-    }
-}
-
-fn animate_light_direction(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<DirectionalLight>>,
-) {
-    for mut transform in &mut query {
-        transform.rotate_y(time.delta_seconds() * 0.5);
     }
 }
 
@@ -205,7 +125,7 @@ struct CubemapMaterial {
 
 impl Material for CubemapMaterial {
     fn fragment_shader() -> ShaderRef {
-        "shaders/cubemap_unlit.wgsl".into()
+        "sky_box/cubemap_unlit.wgsl".into()
     }
 
     fn specialize(
