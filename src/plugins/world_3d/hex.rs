4e75c154af3a144f877c0c859b3e23d31110460e
TristanStreich <tristanstreich@gmail.com>
2023-06-03 14:53:58 -0700

    line between fixed

--- src/plugins/world_3d/hex.rs
+++ src/plugins/world_3d/hex.rs
@@ -6,7 +6,7 @@ use std::cmp::{max,min};
 // Bevy Imports
 use bevy::prelude::*;
 use bevy_inspector_egui::Inspectable;
-use bevy_mod_picking::PickableBundle;
+use bevy_mod_picking::{PickableBundle, PickingEvent, SelectionEvent};
 
 use crate::plugins::world_3d::config::{HEX_CIRCUMRADIUS, HEX_GRID_RADIUS};
 use height_map::{HeightMap, PerlinGenerator, PerlinStep, RandGenerator, FlatGenerator};
@@ -17,7 +17,50 @@ impl Plugin for HexPlugin {
     fn build(&self, app: &mut App) {
         app
         .add_startup_system_to_stage(StartupStage::PreStartup, init_height_map)
-        .add_startup_system(HexGrid::spawn);
+        .add_startup_system(HexGrid::spawn)
+        .add_system(line_drawer);
+    }
+}
+
+fn line_drawer( //TODO: remove this. This is for debugging lines
+    picked_material: Res<PickedMaterial>,
+    mut events: EventReader<PickingEvent>,
+    mut tile_query: Query<(&HexCoord, &mut Handle<StandardMaterial>), With<HexTile>>,
+) {
+    let mut start_line: Option<HexCoord> = None;
+    let mut end_line: Option<HexCoord> = None;
+    // first check all events
+    // looking for tile deselected and tile selected
+    for event in events.iter() {
+        if let PickingEvent::Selection(selection) = event {
+            match selection {
+                SelectionEvent::JustSelected(e) => {
+                    if let Ok((tile_coord, _)) = tile_query.get(*e) {
+                        println!("Selected! {tile_coord:?}");
+                        end_line = Some(tile_coord.clone());
+                    }
+                },
+                SelectionEvent::JustDeselected(e) => {
+                    if let Ok((tile_coord, _)) = tile_query.get(*e) {
+                        println!("Deselected! {tile_coord:?}");
+                        start_line = Some(tile_coord.clone());
+                    }
+                }
+            }
+        }
+    }
+
+    if let (Some(start), Some(end)) = (start_line, end_line) {
+        println!("Got Both");
+        let line = start.line_between(end);
+
+        println!("Found Line: {line:#?}");
+    
+        for (coord, mut material) in tile_query.iter_mut() {
+            if line.contains(coord) {
+                *material = picked_material.0.clone();
+            }
+        }
     }
 }
 
@@ -90,14 +133,14 @@ impl HexCoord {
         ( (self.0 - other.0).abs()
         + (self.0 + self.1 - other.0 - other.1).abs()
         + (self.1 - other.1).abs()
-        ) as u64
+        ) as u64 / 2
     }
 
     /// Gets the hexcoords that draw a straight line between self and other
     /// See: https://www.redblobgames.com/grids/hexagons/#line-drawing
     pub fn line_between(&self, other: HexCoord) -> Vec<HexCoord> {
         let start_world = self.to_world();
-        let end_world = self.to_world();
+        let end_world = other.to_world();
         
         let dist = self.distance(other);
         let mut results = Vec::new();
@@ -131,6 +174,9 @@ impl PartialEq for HexCoord {
 #[derive(Component, Inspectable)]
 pub struct HexGrid;
 
+#[derive(Resource)]
+pub struct PickedMaterial(Handle<StandardMaterial>);
+
  impl HexGrid {
     fn spawn(
         mut commands: Commands,
@@ -138,6 +184,10 @@ pub struct HexGrid;
         mut materials: ResMut<Assets<StandardMaterial>>,
         height_map: Res<HeightMap>
     ) {
+
+        let picked_material = materials.add(Color::rgb(0.,0.,0.8).into()); //TODO: remove. This is for debugging lines
+
+        commands.insert_resource(PickedMaterial(picked_material));
 
         let tile_material = materials.add(Color::rgb(1., 0.8, 0.8).into());
         let hex_tile_mesh: Handle<Mesh> = assets.load("meshes/hex.glb#Mesh0/Primitive0");
